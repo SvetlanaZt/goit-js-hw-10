9ae829a9b6caaaf342c634d5a8073ef8d6d306d1
SvetlanaZt <svetlana.zt23@gmail.com>
2022-05-20 16:26:02 +0300

    dz

--- src/index.js
+++ src/index.js
@@ -10,37 +10,42 @@ const listCountry = document.querySelector('.country-list')
 const coutnryInfo = document.querySelector('.country-info')
 const input = document.getElementById('search-box')
 
+listCountry.style.listStyle = 'none'
+
+
 input.addEventListener('input', debounce(onClick, DEBOUNCE_DELAY))
 
 
 function onClick(e) { 
     const value = e.target.value.trim();
-
+    if (value === '') {
+        coutnryInfo.innerHTML = '';
+        listCountry.innerHTML = '';
+    }
 
     fetchCountries(value)
         .then(renderName)
         .catch(error => { Notiflix.Notify.failure('Oops, there is no country with that name') })
-//          .finally(() => {
-//     input.reset();
-//   });
-        
-}
+        }
 
 function renderName(name) {
     console.log(name.length)
-      if (name.length > 10) {
-      return Notiflix.Notify.info("Too many matches found. Please enter a more specific name.")
-      }
-    else if (name.length === 1) {
-        const addCountry = templateFunction(name[0]);
-        coutnryInfo.innerHTML = addCountry;
+        if (name.length === 1) {
+            const addCountry = templateFunction(name[0]);
+            listCountry.innerHTML = '';
+        return coutnryInfo.innerHTML = addCountry;
     }
-    
-    const listCountrys = name.map(value => {
-    return `<li class="list__item" ${value.flags.svg} ${value.name.official}"></li>`
-}).join("")
+        else if (name.length > 2 && name.length < 10) {
+        const listCountrys = name.map(value => {
+            const createEl = `<li> <img src=${value.flags.svg} alt="Флаг" width="20" height="20"/>${value.name.official}</li>`
+            return createEl
+        }).join("")
 
-listCountry.insertAdjacentHTML('afterbegin', listCountrys)
+        listCountry.insertAdjacentHTML('afterbegin', listCountrys)
+    }
+        else {
+         Notiflix.Notify.info("Too many matches found. Please enter a more specific name.")
+    }
 }
 
 
