8dff3f597d83a706c349be2e05ffe935980fac17
SvetlanaZt <svetlana.zt23@gmail.com>
2022-05-20 12:06:07 +0300

    dz

--- src/index.js
+++ src/index.js
@@ -1,36 +1,43 @@
 import './css/styles.css';
 import templateFunction from './template/country.hbs';
 var debounce = require('lodash.debounce');
+import Notiflix from 'notiflix';
+import { fetchCountries } from './fetchCountries'
+
 const DEBOUNCE_DELAY = 300;
 
 const listCountry = document.querySelector('.country-list')
+const coutnryInfo = document.querySelector('.country-info')
 const input = document.getElementById('search-box')
 
-input.addEventListener('input', onClick)
+input.addEventListener('input', debounce(onClick, DEBOUNCE_DELAY))
 
 
 function onClick(e) { 
-    const value = e.currentTarget.value;
-
-fetchCountries(value)
-    .then(renderName)
-    .cathc(error => console.log(error))
-    .finally(() => {
-        input.reset()
-    })
-
+    const value = e.target.value.trim();
+    // if (value.length < 2) {
+    //         Notiflix.Notify.info("Too many matches found. Please enter a more specific name.")
+    // }
+    fetchCountries(value)
+        .then(renderName)
+        .then(country => console.log(country))
+        // .cathc(specificName)
+            //    
+    // .finally(() => input.reset())
 }
 
-function fetchCountries(name){
-    return fetch(`https://restcountries.com/v3.1/name/${name}`).then(responsive => {
-        return responsive.json()
-    })
-}
+
 
 function renderName(name) {
     console.log(templateFunction(name[0]))
     const addCountry = templateFunction(name[0]);
-    listCountry.innerHTML = addCountry;
+    coutnryInfo.innerHTML = addCountry;
+    
+    const listCountrys = name.map(value => {
+    return `<li class="list__item" ${value.flags.svg} ${value.name.official}"></li>`
+}).join("")
+
+listCountry.insertAdjacentHTML('afterbegin', listCountrys)
 }
 
 
